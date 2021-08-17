04c3cf3d95ce8a02937dabae9b80019457f0a6e9
trojan782 <davidjacobs522@gmail.com>
2021-08-17 23:15:38 +0100

    added the route for the form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home');
 });
+Route::get('/form', [FormController::class, 'form']);

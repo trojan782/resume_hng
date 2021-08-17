de059c5b16d19a2681427673844217cef27dea19
trojan782 <davidjacobs522@gmail.com>
2021-08-17 18:43:44 +0100

    modified the home view

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -15,13 +15,13 @@
     <h1 class="text-3xl">Jacobs David</h1>
     <p class="text-gray-500 pb-4">Software Developer</p>
     <p class="text-gray-700 mb-6"></p>
-
+    <hr>
     <div class="grid grid-cols-1 md:grid-cols-3 grid-flow-row gap-6">
         {{-- <div class="px-4 py-2 bg-blue-600 text-gray-100 rounded-md mr-4 hover:bg-blue-700"><a href="https://twitter.com/digitalocean">Twitter</a></div> --}}
         <div class="px-4 py-2 bg-blue-600 text-gray-100 rounded-md mr-4 hover:bg-blue-700"><a href="https://www.linkedin.com/company/digitalocean">Contact Me</a></div>
         {{-- <div class="px-4 py-2 bg-blue-600 text-gray-100 rounded-md mr-4 hover:bg-blue-700"><a href="https://instagram.com/thedigitalocean">Instagram</a></div> --}}
     </div>
 
-</div> 
+</div>
 </body>
 </html>

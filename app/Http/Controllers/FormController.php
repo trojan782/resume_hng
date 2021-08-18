fe809ed4fd868db8706d50449e8ccb68fd62eabc
trojan782 <davidjacobs522@gmail.com>
2021-08-18 22:05:07 +0100

    fixing bugss

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -14,19 +14,6 @@ class FormController extends Controller
        return view('Form');
    }
 
-//    //Store details
-//    public function contactForm(Request $request) {
-//     //form validation
-//     $this->validate($request,[
-//         'name' => 'required',
-//         'phone' => 'required | regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-//         'email' => 'required | email',
-//         'subject' => 'required | min:5',
-//         'message' => 'required'
-//     ]);
-
-//     //creating a new contact
-//    Contact::create($request->all());
     public function contactForm(Request $request) {
         $details = [
             'name' => $request->name,
@@ -36,21 +23,7 @@ class FormController extends Controller
             'message' => $request->message
         ];
         Mail::to('davidjacobs522@gmail.com')->send(new messageSent($details));
-        return back()->with('Mail_sent', 'Thank You for getting in touch with me'); 
+        return back()->with('Mail_sent', 'Thank You for getting in touch with me');
+        return redirect('/form');
     }
-    //send mail to me
-//     Mail::send('mail', [
-//         'name' => $request->get('name'),
-//         'phone' => $request->get('phone'),
-//         'email' => $request->get('email'),
-//         'subject' => $request->get('subject'),
-//         'user_query' => $request->get('message'),
-//     ],
-//         function($message) use ($request) {
-//             $message->from($request->email);
-//             $message->to('davidjacobs522@gmail.com', 'Developer')->subject($request->get('subject'));
-//    });
-//     return back()->with('Success', 'I have recieved your message, thank you for contacting me');
-//    }
-
 }

d0b6f02e86b5444470d446eea014a95a846ab87b
trojan782 <davidjacobs522@gmail.com>
2021-08-18 12:14:14 +0100

    added the named route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Home');
-});
-Route::get('/form', [FormController::class, 'form']);
+})->name('cv');
+
+Route::get('/form', [FormController::class, 'form'])->name('contact');

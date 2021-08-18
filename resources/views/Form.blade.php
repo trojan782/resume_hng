fd8516cee12dff53736f6673b982d90d6a449206
trojan782 <davidjacobs522@gmail.com>
2021-08-18 08:50:02 +0100

    changed the bg color of the form and added opacity

--- resources/views/Form.blade.php
+++ resources/views/Form.blade.php
@@ -12,7 +12,7 @@
 
 <body class="bg-gradient-to-r from-blue-400 via-purple-600 to-blue-700">
 
-    <div class="w-3/4 lg:w-1/2 mx-auto  rounded-md bg-gray-200 shadow-lg m-20 p-6 text-center">
+    <div class="w-3/4 lg:w-1/2 mx-auto  rounded-md bg-white opacity-78 shadow-lg m-20 p-6 text-center">
 
         <h1 class="text-3xl">Contact Form</h1>
         <p class="text-gray-500 pb-4">Feel free to contact me</p>
@@ -23,22 +23,24 @@
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                             for="grid-first-name">
-                            First Name
+                            Name
                         </label>
                         <input
                             class="appearance-none block w-full bg-white-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                            id="grid-first-name" type="text" placeholder="John">
+                            id="grid-first-name" type="text" placeholder="John Doe">
                     </div>
+
                     <div class="w-full md:w-1/2 px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                             for="grid-last-name">
-                            Last Name
+                            Phone
                         </label>
                         <input
                             class="appearance-none block w-full bg-white-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-last-name" type="text" placeholder="Doe">
+                            id="grid-last-name" type="tel" placeholder="Telphone Number">
                     </div>
                 </div>
+
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -48,9 +50,22 @@
                         <input
                             class="appearance-none block w-full bg-white-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                             id="email" type="email">
+                    </div>
+                </div>
+
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            for="grid-password">
+                            Subject
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-white-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="email" type="email">
 
                     </div>
                 </div>
+
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -70,7 +85,9 @@
                 </div>
             </form>
         </form>
-
+    </div>
+    <div class="w-3/4 lg:w-1/2 mx-auto  rounded-md shadow-lg m-10 p-6 text-center">
+        <a href="">Take me back 👈🏼</a>
     </div>
 </body>
 
